f620a2ebbb35df9ea03209f7233f178037bd4eb6
paramthakkar123 <paramthakkar864@gmail.com>
2025-10-28 15:58:56 +0530

    Updates

--- docs/make.jl
+++ docs/make.jl
@@ -5,19 +5,27 @@ DocMeta.setdocmeta!(HealthSampleData, :DocTestSetup, :(using HealthSampleData); 
 
 makedocs(;
     modules=[HealthSampleData],
-    authors="TheCedarPrince <jacobszelko@gmail.com> and contributors",
+    checkdocs = :none,
+    authors="TheCedarPrince <jacobszelko@gmail.com>, ParamThakkar123 <paramthakkar864@gmail.com> and contributors",
+    repo = "https://github.com/JuliaHealth/HealthSampleData.jl/blob/{commit}{path}#{line}",
     sitename="HealthSampleData.jl",
     format=Documenter.HTML(;
-        canonical="https://TheCedarPrince.github.io/HealthSampleData.jl",
-        edit_link="master",
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical="https://JuliaHealth.github.io/HealthSampleData.jl",
         assets=String[],
     ),
     pages=[
         "Home" => "index.md",
+        "Quickstart" => "quickstart.md",
+
+        "Pre-Cohort Analysis" => "precohort.md",
+        "Post-Cohort Analysis" => "postcohort.md",
+        
+        "API" => "api.md",
     ],
+    doctest = false,
 )
 
 deploydocs(;
-    repo="github.com/TheCedarPrince/HealthSampleData.jl",
-    devbranch="master",
+    repo="github.com/JuliaHealth/HealthSampleData.jl",
 )
