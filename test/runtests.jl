9047a363aefe25a7c78c77203c2948b42f463069
paramthakkar123 <paramthakkar864@gmail.com>
2025-11-12 00:11:27 +0530

    Removed tests for progress callback

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,6 @@ using HealthSampleData
 using Test
 
 @testset "Utilities" begin
-    @testset "progress_callback" begin
-        last_pct = Ref(-1)
-        @test HealthSampleData.progress_callback(512 * 1024^2, 1024 * 1024^2, last_pct) === nothing
-        @test last_pct[] == 50  # 50% progress
-
-        @test HealthSampleData.progress_callback(1024 * 1024^2, 1024 * 1024^2, last_pct) === nothing
-        @test last_pct[] == 100  # 100% progress
-
-        @test HealthSampleData.progress_callback(512 * 1024^2, 0, last_pct) === nothing  # Unknown total size
-    end
 end
 
 @testset "HuggingFaceDatasets - Test dataset" begin
