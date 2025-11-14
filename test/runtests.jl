6b0b299e7b0cf4cef52398d27da2dfe97e60372e
paramthakkar123 <paramthakkar864@gmail.com>
2025-11-14 19:13:04 +0530

    added tests for Test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,16 @@
 using HealthSampleData
 using Test
 
-@testset "Utilities" begin
-end
-
-@testset "HuggingFaceDatasets - Test dataset" begin
+@testset "HealthSampleData.jl" begin
     @test isa(HealthSampleData.Test, Function)
-    @test hasmethod(HealthSampleData.Test, Tuple{}) 
+    @test hasmethod(HealthSampleData.Test, Tuple{})
 
     @test isa(HealthSampleData.download_hf_dataset, Function)
 
     @test_throws ErrorException HealthSampleData.download_hf_dataset("NonExistentDataset12345")
+    @testset "HuggingFaceDatasets - Test dataset download" begin
+        path = HealthSampleData.download_hf_dataset("Test")
+        @test isa(path, String)
+        @test path == "Test/penguins.csv"
+    end
 end
